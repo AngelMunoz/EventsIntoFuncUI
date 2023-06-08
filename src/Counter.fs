a0ffeb30ffa61e857f37675a6b9abf16f44b55a4
Angel D. Munoz <scythes.lol@gmail.com>
2023-06-07 19:57:42 -0600

    first commit

---
+++ src/Counter.fs
@@ -0,0 +1,48 @@
+﻿namespace WindowSample
+
+module Counter =
+  open Avalonia.Controls
+  open Avalonia.FuncUI
+  open Avalonia.FuncUI.DSL
+  open Avalonia.Layout
+  open Avalonia.FuncUI.Builder
+  open System
+
+
+  let view
+    (
+      initialHeight: float,
+      initialWidth: float,
+      windowSizeChanged: IObservable<float * float>
+    ) =
+
+    Component(fun ctx ->
+
+      // set the initial size of the window
+      let width = ctx.useState initialWidth
+      let height = ctx.useState initialHeight
+      // register the window size changed event with a use effect hook
+      ctx.useEffect(
+        (fun _ ->
+          // subscribe to the window size changed event
+          windowSizeChanged
+          |> Observable.subscribe(fun (newWidth, newHeight) ->
+            // update the state value with the new size
+            width.Set(newWidth)
+            height.Set(newHeight)
+          )
+        ),
+        [ EffectTrigger.AfterInit ]
+      )
+
+      StackPanel.create [
+        StackPanel.orientation Orientation.Horizontal
+        StackPanel.verticalAlignment VerticalAlignment.Center
+        StackPanel.horizontalAlignment HorizontalAlignment.Center
+        StackPanel.spacing 12.
+        StackPanel.children [
+          TextBlock.create [ TextBlock.text "Window size: " ]
+          TextBlock.create [ TextBlock.text $"%f{width.Current} x %f{height.Current}" ]
+        ]
+      ]
+    )
---
+++ src/Program.fs
@@ -0,0 +1,44 @@
+﻿namespace WindowSample
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Input
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+
+type MainWindow() as this =
+  inherit HostWindow()
+
+  let windowSizeObs =
+    this.SizeChanged
+    |> Observable.map(fun event -> event.NewSize.Width, event.NewSize.Height)
+
+  let width, height = 400.0, 400.0
+
+  do
+    base.Title <- "WindowSample"
+    base.Width <- width
+    base.Height <- height
+    // to avoid coupling your code to the Window,
+    // just pass an observable which is agnostic of the window
+    this.Content <- Counter.view(height, width, windowSizeObs)
+
+type App() =
+  inherit Application()
+
+  override this.Initialize() = this.Styles.Add(FluentTheme())
+
+  override this.OnFrameworkInitializationCompleted() =
+    match this.ApplicationLifetime with
+    | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+      desktopLifetime.MainWindow <- MainWindow()
+    | _ -> ()
+
+module Program =
+
+  [<EntryPoint>]
+  let main (args: string[]) =
+    AppBuilder
+      .Configure<App>()
+      .UsePlatformDetect()
+      .StartWithClassicDesktopLifetime(args)
